7cebfe1cdbc6ae01a5bd393b44b5e832fcecac01
admin <admin@MOUNTAIN>
2021-01-08 13:25:33 +0530

    car data

---
+++ Database/Database/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,13 @@
+﻿
+maruti,2000
+discover,22000
+
+GO
+
+--Syntax Error: Incorrect syntax near maruti.
+--maruti,2000
+--discover,22000
+
+
+
+GO
---
+++ Database/Database/dbo/Tables/car.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[car] (
+    [name]   VARCHAR (10) NULL,
+    [number] VARCHAR (10) NULL
+);
+
